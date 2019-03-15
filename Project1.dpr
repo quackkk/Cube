09e58a4826c0eca6b8bc54dd58b8b7d8913b87b8
V <qargvo@gmail.com>
2019-03-15 22:48:01 +0300

    end_of_initialization

--- Project1.dpr
+++ Project1.dpr
@@ -8,9 +8,11 @@ const
   hos = 1000;
 type
   tm = array of array [1..2] of ShortInt;
+  sm = array of array of ShortInt;
+  bm = array of Boolean;
   log_record = record
   ms:tm;
-  move_list:string[20];
+  move_list:string;
   ind_move:Byte;
   ind_cube:Byte;
   sum_weight:Word;
@@ -18,16 +20,29 @@ type
   var
     log_rec:array of log_record;
     n:Byte;
-    i:integer;
-
+    i,j:integer;
+    solve_arr:bm;
 begin
   while True do
   begin
     Write('INPUT size of cube arr: ');
     Readln(n);
     SetLength(log_rec,hos);
+    SetLength(solve_arr, n + 2);
+    for i:=1 to n do
+      solve_arr[i]:=False;
+    solve_arr[0]:=True;
+    solve_arr[n+1]:= True;
     for i := 0 to hos-1 do
       SetLength(log_rec[i].ms,n);
+    for i := 0 to n - 1 do
+      readln(log_rec[0].ms[i,1]);
+    j:=0;
+    while j >= 0 do
+      begin
+
+
+      end;
 
 
 
